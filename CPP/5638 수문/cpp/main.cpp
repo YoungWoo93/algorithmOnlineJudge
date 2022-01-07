850aee72d000be983f8c72d4d35d7ed0271a5fcb
leeyongwoo93 <rmstjd23@gmail.com>
2022-01-07 12:34:22 +0900

    커밋 환경세팅

---
+++ CPP/5638 수문/cpp/main.cpp
@@ -0,0 +1,24 @@
+#include <problem.h>
+
+#ifdef _5638_
+
+/// 
+/// problem
+/// https://www.acmicpc.net/problem/5638
+/// 
+/// solution
+/// 
+/// 
+
+
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+void main()
+{
+	cout << "hi" << endl;
+}
+
+#endif
