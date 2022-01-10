14e10b0c0159e66e2dc6eef601c5e6c6e4e63e55
leeyongwoo93 <rmstjd23@gmail.com>
2022-01-11 04:00:46 +0900

    2014

---
+++ code/2014 소수의 곱/2014.cpp
@@ -0,0 +1,78 @@
+#include <problem.h>
+
+#ifdef _2014_
+
+/// 
+/// problem
+/// https://www.acmicpc.net/problem/2014
+/// 
+/// solution
+/// 
+/// 
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+struct primeNumber {
+	int value;
+	priority_queue<unsigned long long int, vector<unsigned long long int>, greater<unsigned long long int>> pq;
+};
+
+vector<primeNumber> input(int& endIndex)
+{
+	int primeNumberCount;
+
+	cin >> primeNumberCount;
+	cin >> endIndex;
+
+	vector<primeNumber> ret;
+	for (int i = 0; i < primeNumberCount; i++)
+	{
+		primeNumber temp;
+		cin >> temp.value;
+
+		ret.push_back(temp);
+	}
+
+	return ret;
+}
+
+int main()
+{
+	int endIndex;
+	auto primeNumberList = input(endIndex);
+
+	vector<unsigned long long int> result;
+	result.push_back(1);
+
+	while (result.size() != endIndex + 1)
+	{
+		unsigned long long int tempValue = pow(2, 64);
+		int tempIndex;
+
+		for (int i=0; i<primeNumberList.size(); i++)
+		{
+			primeNumberList[i].pq.push(result.back());
+			if (primeNumberList[i].pq.top() * primeNumberList[i].value == result.back())
+				primeNumberList[i].pq.pop();
+
+			if (primeNumberList[i].pq.top() * primeNumberList[i].value < tempValue)
+			{
+				tempValue = primeNumberList[i].pq.top() * primeNumberList[i].value;
+				tempIndex = i;
+			}
+		}
+
+		result.push_back(primeNumberList[tempIndex].pq.top() * primeNumberList[tempIndex].value);
+		primeNumberList[tempIndex].pq.pop();
+	}
+
+	cout << result.back();
+	return 0;
+}
+#endif
