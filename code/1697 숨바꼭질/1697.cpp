b04b479d03839f77fea6187142864c639a85de2a
leeyongwoo93 <rmstjd23@gmail.com>
2022-01-11 03:02:48 +0900

    1697 fix

--- code/1697 숨바꼭질/1697.cpp
+++ code/1697 숨바꼭질/1697.cpp
@@ -29,55 +29,52 @@ vector<int> input(int& start, int& end)
 
 int main()
 {
-	while (true)
+	int start;
+	int end;
+	auto board = input(start, end);
+
+	if (end <= start)
 	{
-
-		int start;
-		int end;
-		auto board = input(start, end);
-
-		if (end <= start)
-		{
-			cout << start - end << endl;
-			return 0;
-		}
-
-		queue<int> q;
-		q.push(start);
-		board[start] = 0;
-
-		while (board[end] == -1)
-		{
-			if (q.front() + 1 < board.size())
-			{
-				if (board[q.front() + 1] == -1 || board[q.front() + 1] > board[q.front()] + 1)
-				{
-					board[q.front() + 1] = board[q.front()] + 1;
-					q.push(q.front() + 1);
-				}
-			}
-			if (q.front() - 1 >= 0)
-			{
-				if (board[q.front() - 1] == -1 || board[q.front() - 1] > board[q.front()] + 1)
-				{
-					board[q.front() - 1] = board[q.front()] + 1;
-					q.push(q.front() - 1);
-				}
-			}
-			if (q.front() * 2 < board.size())
-			{
-				if (board[q.front() * 2] == -1 || board[q.front() * 2] > board[q.front()] + 1)
-				{
-					board[q.front() * 2] = board[q.front()] + 1;
-					q.push(q.front() * 2);
-				}
-			}
-
-			q.pop();
-		}
-
-		cout << board[end] << endl;
+		cout << start - end << endl;
+		return 0;
 	}
+
+	queue<int> q;
+	q.push(start);
+	board[start] = 0;
+
+	while (board[end] == -1)
+	{
+		if (q.front() + 1 < board.size())
+		{
+			if (board[q.front() + 1] == -1 || board[q.front() + 1] > board[q.front()])
+			{
+				board[q.front() + 1] = board[q.front()] + 1;
+				q.push(q.front() + 1);
+			}
+		}
+		if (q.front() - 1 >= 0)
+		{
+			if (board[q.front() - 1] == -1 || board[q.front() - 1] > board[q.front()])
+			{
+				board[q.front() - 1] = board[q.front()] + 1;
+				q.push(q.front() - 1);
+			}
+		}
+		if (q.front() * 2 < board.size())
+		{
+			if (board[q.front() * 2] == -1 || board[q.front() * 2] > board[q.front()])
+			{
+				board[q.front() * 2] = board[q.front()] + 1;
+				q.push(q.front() * 2);
+			}
+		}
+
+		q.pop();
+	}
+
+	cout << board[end] << endl;
+
 	return 0;
 }
 #endif
