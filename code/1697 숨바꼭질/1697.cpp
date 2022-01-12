4204cfd978e3d893174fdc555cdabf9d07024c5d
leeyongwoo93 <rmstjd23@gmail.com>
2022-01-12 18:35:12 +0900

    1697 fix

--- code/1697 숨바꼭질/1697.cpp
+++ code/1697 숨바꼭질/1697.cpp
@@ -10,6 +10,75 @@
 /// 
 /// 
 
+//#include <iostream>
+//#include <vector>
+//#include <queue>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//vector<int> input(int& start, int& end)
+//{
+//	cin >> start;
+//	cin >> end;
+//
+//	vector<int> ret(end * 2, -1);
+//
+//	return ret;
+//}
+//
+//int main()
+//{
+//	int start;
+//	int end;
+//	auto board = input(start, end);
+//
+//	if (end <= start)
+//	{
+//		cout << start - end << endl;
+//		return 0;
+//	}
+//
+//	queue<int> q;
+//	q.push(start);
+//	board[start] = 0;
+//
+//	while (board[end] == -1)
+//	{
+//		if (q.front() + 1 < board.size())
+//		{
+//			if (board[q.front() + 1] == -1 || board[q.front() + 1] > board[q.front()])
+//			{
+//				board[q.front() + 1] = board[q.front()] + 1;
+//				q.push(q.front() + 1);
+//			}
+//		}
+//		if (q.front() - 1 >= 0)
+//		{
+//			if (board[q.front() - 1] == -1 || board[q.front() - 1] > board[q.front()])
+//			{
+//				board[q.front() - 1] = board[q.front()] + 1;
+//				q.push(q.front() - 1);
+//			}
+//		}
+//		if (q.front() * 2 < board.size())
+//		{
+//			if (board[q.front() * 2] == -1 || board[q.front() * 2] > board[q.front()])
+//			{
+//				board[q.front() * 2] = board[q.front()] + 1;
+//				q.push(q.front() * 2);
+//			}
+//		}
+//
+//		q.pop();
+//	}
+//
+//	cout << board[end] << endl;
+//
+//	return 0;
+//}
+
+
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -40,10 +109,10 @@ int main()
 	}
 
 	queue<int> q;
-	q.push(start);
-	board[start] = 0;
+	q.push(end);
+	board[end] = 0;
 
-	while (board[end] == -1)
+	while (board[start] == -1)
 	{
 		if (q.front() + 1 < board.size())
 		{
@@ -61,19 +130,19 @@ int main()
 				q.push(q.front() - 1);
 			}
 		}
-		if (q.front() * 2 < board.size())
+		if (q.front() % 2 == 0 && q.front() / 2 >= 0)
 		{
-			if (board[q.front() * 2] == -1 || board[q.front() * 2] > board[q.front()])
+			if (board[q.front() / 2] == -1 || board[q.front() / 2] > board[q.front()])
 			{
-				board[q.front() * 2] = board[q.front()] + 1;
-				q.push(q.front() * 2);
+				board[q.front() / 2] = board[q.front()] + 1;
+				q.push(q.front() / 2);
 			}
 		}
 
 		q.pop();
 	}
 
-	cout << board[end] << endl;
+	cout << board[start] << endl;
 
 	return 0;
 }
