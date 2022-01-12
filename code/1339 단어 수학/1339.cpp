b859a190ff71950d2cf7f365e5fcd2ef2197f8e6
leeyongwoo93 <rmstjd23@gmail.com>
2022-01-12 18:35:25 +0900

    1339

---
+++ code/1339 단어 수학/1339.cpp
@@ -0,0 +1,74 @@
+#include <problem.h>
+
+#ifdef _1339_
+
+/// 
+/// problem
+/// https://www.acmicpc.net/problem/1339
+/// 
+/// solution
+/// 
+/// 
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <algorithm>
+
+
+using namespace std;
+
+vector<vector<int>>input()
+{
+	vector<vector<int>> ret(8, vector<int>('Z' - 'A' + 1, 0));
+	int words;
+	string temp;
+	cin >> words;
+
+	for (int i = 0; i < words; i++)
+	{
+		cin >> temp;
+		for (int i = 0; i < temp.length(); i++)
+		{
+			ret[i][temp[temp.length() - i - 1] - 'A']++;
+		}
+	}
+
+	return ret;
+}
+
+
+int main()
+{
+	auto wordList = input();
+	vector<unsigned long long int> weightList('Z' - 'A' + 1, 0);
+
+	for (int i = 0; i < wordList.size(); i++)
+	{
+		for (int j=0; j<wordList[i].size(); j++)
+		{
+			weightList[j] += pow(10, i) * wordList[i][j];
+		}
+	}
+
+	priority_queue<unsigned long long int> pq;
+
+	for (auto item : weightList)
+		pq.push(item);
+
+	int offset = 9;
+	unsigned long long int result = 0;
+	while (!pq.empty())
+	{
+		result += offset * pq.top();
+		offset--;
+		pq.pop();
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
+#endif
