aa5759821a3df138380058c690990a2738476c1b
leeyongwoo93 <rmstjd23@gmail.com>
2022-01-18 16:30:26 +0900

    1463

---
+++ code/1463 1로 만들기/1463.cpp
@@ -0,0 +1,60 @@
+#include <problem.h>
+
+#ifdef _1463_
+
+/// 
+/// problem
+/// https://www.acmicpc.net/problem/1463
+/// 
+/// solution
+/// 
+/// 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+
+int main()
+{
+	int num;
+	cin >> num;
+
+	vector<int> lst(num*2, -1);
+	lst[1] = 0;
+	queue<int> q;
+	
+	q.push(1);
+
+	while (lst[num] == -1)
+	{
+		auto temp = q.front();
+
+		if (temp + 1 <= num && lst[temp + 1] == -1)
+		{
+			q.push(temp + 1);
+			lst[temp + 1] = lst[temp] + 1;
+		}
+		if (temp * 2 <= num && lst[temp * 2] == -1)
+		{
+			q.push(temp * 2);
+			lst[temp * 2] = lst[temp] + 1;
+		}
+		if (temp * 3 <= num && lst[temp * 3] == -1)
+		{
+			q.push(temp * 3);
+			lst[temp *3] = lst[temp] + 1;
+		}
+
+		q.pop();
+	}
+
+	cout << lst[num];
+
+	return 0;
+}
+
+#endif
