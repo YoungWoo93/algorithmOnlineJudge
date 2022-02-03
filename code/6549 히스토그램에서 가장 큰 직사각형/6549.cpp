bd1baa36f3d09d094b4a22f5d2df5300df85b1de
leeyongwoo93 <rmstjd23@gmail.com>
2022-02-04 04:26:06 +0900

    6549

--- code/6549 히스토그램에서 가장 큰 직사각형/6549.cpp
+++ code/6549 히스토그램에서 가장 큰 직사각형/6549.cpp
@@ -12,134 +12,114 @@
 
 #include <iostream>
 #include <vector>
-
+#include <queue>
+#include <algorithm>
+#include <limits.h>
+#include <cmath>
 using namespace std;
 
-typedef struct node {
-	unsigned long long int value;
-	unsigned long long int index;
-
-	node* left;
-	node* right;
-}node;
-
-bool input(vector<int>* arr);
-node* getSegmentTree(int start, int end, vector<int>* arr);
-node* getMinIndex(int leftBoundery, int rightBoundery, int start, int end, node* root);
-unsigned long long int getMaxArea(int leftBoundery, int rightBoundery, int start, int end, node* root);
-
-
-bool input(vector<int>* arr)
+int getTreeSize(int n)
 {
-	int count;
-	cin >> count;
+	return pow(2, ceil(log2(n)));
+}
 
-	if (count == 0)
-		return false;
-	arr->clear();
+vector<int> input(int count)
+{
+	vector<int> ret(count, 0);
 
 	for (int i = 0; i < count; i++)
 	{
-		int temp;
-		cin >> temp;
-
-		arr->push_back(temp);
-	}
-	arr->push_back(0);
-	return true;
-}
-
-node* getSegmentTree(int start, int end, vector<int>* arr)
-{
-	if (start == end)
-	{
-		node* ret = new node();
-		ret->value = arr->at(start);
-		ret->index = start;
-		ret->left = NULL;
-		ret->right = NULL;
-
-		return ret;
+		cin >> ret[i];
 	}
 
-	node* ret = new node();
-	node* left = getSegmentTree(start, (start + end) / 2, arr);
-	node* right = getSegmentTree((start + end) / 2 + 1, end, arr);
-
-	ret->value = (left->value < right->value ? left->value : right->value);
-	ret->index = (left->value < right->value ? left->index : right->index);
-
-	ret->left = left;
-	ret->right = right;
 
 	return ret;
 }
 
-node* getMinIndex(int leftBoundery, int rightBoundery, int start, int end, node* root)
+
+pair<unsigned long long int, int> makeSegmentTree(vector<pair<unsigned long long int, int>>& arr, int cur)
 {
+	if (cur * 2 < arr.size())
+	{
+		auto left = makeSegmentTree(arr, cur * 2);
+		auto right = makeSegmentTree(arr, cur * 2 + 1);
+		arr[cur] = left.first < right.first ? left : right;
+	}
+
+	return arr[cur];
+}
+
+vector<pair<unsigned long long int, int>> getSegmentTree(vector<int>& arr)
+{
+	int size = getTreeSize(arr.size());
+	vector<pair<unsigned long long int, int>> ret(size * 2, pair<unsigned long long int, int>(LLONG_MAX, 0));
+
+
+	for (int i = 0; i < arr.size(); i++)
+	{
+		ret[size + i].first = arr[i];
+		ret[size + i].second = i;
+	}
+
+	makeSegmentTree(ret, 1);
+
+	return ret;
+}
+
+pair<unsigned long long int, int> getMinIndex(vector<pair<unsigned long long int, int>>& histogram, int start, int end, int cur, int rangeStart, int rangeEnd)
+{
+	if (rangeEnd < start || end < rangeStart)
+		return pair<unsigned long long int, int>(LLONG_MAX, 0);
+
+	if (rangeStart <= start && end <= rangeEnd)
+		return histogram[cur];
+
 	int mid = (start + end) / 2;
-	node* r;
-	node* ret;
-
-	ret = (node*)NULL;
-
-	if (start > rightBoundery || end < leftBoundery)
-		return ret;
-	if (start >= leftBoundery && end <= rightBoundery)
-		return root;
-
-
-
-	if (leftBoundery <= mid)
-		ret = getMinIndex(leftBoundery, rightBoundery, start, mid, root->left);
-
-
-	if (rightBoundery > mid)
-	{
-		r = getMinIndex(leftBoundery, rightBoundery, mid + 1, end, root->right);
-
-		if (ret == NULL)
-			ret = r;
-		else
-		{
-			ret = (ret->value < r->value ? ret : r);
-		}
-
-	}
-
-	return ret;
+	auto left = getMinIndex(histogram, start, mid, cur * 2, rangeStart, rangeEnd);
+	auto right = getMinIndex(histogram, mid + 1, end, cur * 2 + 1, rangeStart, rangeEnd);
+	return left.first < right.first ? left : right;
 }
 
-unsigned long long int getMaxArea(int leftBoundery, int rightBoundery, int start, int end, node* root)
+unsigned long long int getMaxSize(vector<pair<unsigned long long int, int>>& histogram, int start, int end, int cur, int rangeStart, int rangeEnd)
 {
-	node* tempNode = getMinIndex(leftBoundery, rightBoundery, start, end, root);
-	if (tempNode == NULL)
-		return 0;
+	if (rangeStart == rangeEnd)
+		return histogram[rangeStart].first;
 
-	unsigned long long int tempArea = tempNode->value * (rightBoundery - leftBoundery + 1);
-	//	cout << "[" << leftBoundery - 1 << "~" << rightBoundery - 1 << "] 구간 최소값 [" << tempNode->value << "] 이용 " << tempArea << endl;
+	unsigned long long int ret = 0;
 
-	unsigned long long int leftArea = getMaxArea(leftBoundery, tempNode->index, start, end, root);
-	unsigned long long int rightArea = getMaxArea(tempNode->index + 2, rightBoundery, start, end, root);
+	int minIndex = getMinIndex(histogram, start, end, cur, rangeStart, rangeEnd).second + start;
 
-	if (tempArea < leftArea)
-		tempArea = leftArea;
-	if (tempArea < rightArea)
-		tempArea = rightArea;
 
-	return tempArea;
+	vector<unsigned long long int> maxValue;
+	if (minIndex != rangeStart)
+		maxValue.push_back(getMaxSize(histogram, start, end, cur, rangeStart, minIndex - 1));
+
+	if (minIndex != rangeEnd)
+		maxValue.push_back(getMaxSize(histogram, start, end, cur, minIndex + 1, rangeEnd));
+
+	maxValue.push_back((rangeEnd - rangeStart + 1) * histogram[minIndex].first);
+
+	return *max_element(maxValue.begin(), maxValue.end());
 }
 
 int main()
 {
-	vector<int> arr;
-
-	while (input(&arr))
+	while (true)
 	{
-		node* root = getSegmentTree(0, arr.size() - 1, &arr);
+		int count;
+		cin >> count;
 
-		cout << getMaxArea(1, arr.size(), 1, arr.size(), root) << "\n";
+		if (count == 0)
+			break;
+
+		auto histogram = input(count);
+		auto segmentTree = getSegmentTree(histogram);
+		int size = getTreeSize(count);
+
+		cout << getMaxSize(segmentTree, size, segmentTree.size() - 1, 1, size, size + count - 1) << endl;
 
 	}
+	return 0;
 }
 #endif
+
