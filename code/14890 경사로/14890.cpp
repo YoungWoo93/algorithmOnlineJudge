0717a2d1a972607f16963a837d9e91e68bf2d75d
leeyongwoo93 <rmstjd23@gmail.com>
2022-02-04 05:41:02 +0900

    14890

---
+++ code/14890 경사로/14890.cpp
@@ -0,0 +1,119 @@
+#include <problem.h>
+
+#ifdef _14890_
+
+/// 
+/// problem
+/// https://www.acmicpc.net/problem/14890
+/// 
+/// solution
+/// 
+/// 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<vector<int>> input(int& length)
+{
+	int size;
+	cin >> size >> length;
+
+	vector<vector<int>> ret(size, vector<int>(size, 0));
+
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			cin >> ret[i][j];
+		}
+	}
+
+	return ret;
+}
+
+int travel(vector<vector<int>>& map, int length)
+{
+	int ret = 0;
+	
+	for (int i = 0; i < map.size(); i++)
+	{
+		int flatCount = 0;
+		for (int j = 0; j < map[i].size() - 1; j++)
+		{
+			flatCount++;
+
+			if (map[i][j] < map[i][j + 1])
+			{
+				if (flatCount < length || abs(map[i][j] - map[i][j + 1]) > 1)
+				{
+					flatCount = -2;
+					break;
+				}
+
+				flatCount = 0;
+			}
+			if (map[i][j] > map[i][j + 1])
+			{
+				if (flatCount < 0 || abs(map[i][j] - map[i][j + 1]) > 1)
+				{
+					flatCount = -2;
+					break;
+				}
+				flatCount = -length;
+			}
+		}
+		flatCount++;
+
+		if (flatCount >= 0)
+			ret++;
+	}
+
+	for (int i = 0; i < map.size(); i++)
+	{
+		int flatCount = 0;
+		for (int j = 0; j < map[i].size() - 1; j++)
+		{
+			flatCount++;
+
+			if (map[j][i] < map[j + 1][i])
+			{
+				if (flatCount < length || abs(map[j][i] - map[j + 1][i]) > 1)
+				{
+					flatCount = -2;
+					break;
+				}
+
+				flatCount = 0;
+			}
+			if (map[j][i] > map[j + 1][i])
+			{
+				if (flatCount < 0 || abs(map[j][i] - map[j + 1][i]) > 1)
+				{
+					flatCount = -2;
+					break;
+				}
+				flatCount = -length;
+			}
+		}
+		flatCount++;
+
+		if (flatCount >= 0)
+			ret++;
+	}
+
+	return ret;
+}
+
+int main()
+{
+	int length = 0;
+	auto map = input(length);
+	cout << travel(map, length) << endl;
+
+	return 0;
+}
+
+#endif
