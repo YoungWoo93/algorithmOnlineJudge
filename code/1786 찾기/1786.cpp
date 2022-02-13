cba4314af101af7d0fad36f161f286e8f6a6b3ce
leeyongwoo93 <rmstjd23@gmail.com>
2022-02-14 06:42:02 +0900

    1786

---
+++ code/1786 찾기/1786.cpp
@@ -0,0 +1,105 @@
+#include <problem.h>
+
+#ifdef _1786_
+
+/// 150
+/// problem
+/// https://www.acmicpc.net/problem/1786
+/// 
+/// solution
+/// 
+/// 
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+vector<string> input()
+{
+	string temp;
+	vector<string> ret;
+
+	getline(cin, temp);
+	ret.push_back(temp);
+	getline(cin, temp);
+	ret.push_back(temp);
+
+	return ret;
+}
+
+vector<int> preprocessing(string pattern)
+{
+	vector<int> ret(pattern.length(), 0);
+	int cur = 0;
+	
+	for (int i = 1; i < pattern.length(); i++)
+	{
+		if (pattern.c_str()[cur] != pattern.c_str()[i])
+		{
+			if (cur != 0)
+			{
+				cur = ret[cur - 1];
+				i--;
+				continue;
+			}
+		}
+		else
+			cur++;
+
+		ret[i] = cur;
+	}
+
+	return ret;
+}
+
+vector<int> matching(string str, string pattern, vector<int>& preprocessTable)
+{
+	vector<int> ret;
+	int count = 0;
+	int offset = 0;
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str.c_str()[i] == pattern.c_str()[offset])
+		{
+			offset++;
+			if (offset == pattern.length())
+			{
+				count++;
+				ret.push_back(i - offset + 1);
+				offset = *(preprocessTable.end()-1);
+			}
+		}
+		else if (offset != 0)
+		{
+			i--;
+			offset = preprocessTable[offset - 1];
+		}
+
+	}
+
+	ret.insert(ret.begin(), count);
+	return ret;
+}
+
+int main()
+{
+	auto strs = input();
+	auto pre = preprocessing(strs[1]);
+	auto result = matching(strs[0], strs[1], pre);
+
+	cout << result[0] << endl;
+
+	if (result.size() > 1)
+		cout << result[1] + 1;
+	for (int i = 2; i < result.size(); i++)
+	{
+		cout << " " << result[i] + 1;
+	}
+
+	return 0;
+}
+#endif
